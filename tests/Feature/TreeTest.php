21265dbb85bb9f9bfec300cf258b175a93521a2c
Viktor Bachmanov <vbachmanov@mail.ru>
2023-12-14 13:32:58 +0300

    Create a 'tests/Feature/TreeTest'

---
+++ tests/Feature/TreeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+use App\Models\User;
+
+
+class TreeTest extends TestCase
+{
+    // use WithoutMiddleware;
+
+    /**
+     *  @test
+     */
+    public function it_gets_tree_for_auth_user(): void
+    {
+        $user = User::factory()->create();
+        
+        $response = $this->actingAs($user)
+            ->getJson('/api/pw-storage/tree');
+
+        $response->dumpHeaders();
+        $response->dump();
+
+        $response->assertStatus(200);
+    }
+}
